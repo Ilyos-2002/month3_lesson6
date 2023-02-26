287f1f59f5c8e7440457bbd3497572feb3e0bcb7
Ilyos-2002 <ilyosxolmamatov3@gmail.com>
2023-02-26 15:55:34 +0500

    Add code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
  var userMoney = prompt("Hisobingizdagi pul miqdorini so'mda kiriting?");
- var moneyYev = 11650.34;
+ var moneyYev = 11650.03;
  var moneyDol = 10650.34;
  var moneyTotal = 500 * moneyDol + 250 * moneyDol + 120 * moneyYev;
  var saytText = document.querySelector("p");
 
- if (0 < userMoney && userMoney > moneyTotal) {
-     saytText.textContent = " Oq yo'l,Alisher!"
+ if (userMoney >= moneyTotal) {
+     saytText.textContent = " Oq yo'l,Alisher! Sen uddalading."
 
- } else if (0 < userMoney && userMoney < moneyTotal) {
+ } else if (userMoney < moneyTotal) {
      saytText.textContent = "Alisher, ozgina sabr qilish kerak bo'lar ekan."
 
  } else {
